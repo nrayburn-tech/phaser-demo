28582df2fc8323ee98347ab0307fbc6399df0e2a
Nick Rayburn <52075362+nrayburn-tech@users.noreply.github.com>
2022-03-22 15:21:56 -0500

    implement resizing with arrow keys

--- src/DemoGame.tsx
+++ src/DemoGame.tsx
@@ -32,8 +32,10 @@ class DemoGame extends Phaser.Game {
 class DemoScene extends Phaser.Scene {
     declare game: DemoGame;
     private BACKGROUND_KEY = 'background';
+    private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+    private cursorText!: Phaser.GameObjects.Text;
     private background!: Phaser.GameObjects.TileSprite;
-
+    private originalSize!: { width: number; height: number };
 
     constructor(key: string) {
         super(key);
@@ -47,6 +49,40 @@ class DemoScene extends Phaser.Scene {
         this.physics.world.setBounds(0, 0, this.scale.width, this.scale.height);
         this.cameras.resize(this.scale.width, this.scale.height);
         this.background = this.add.tileSprite(0, 0, this.scale.width, this.scale.height, this.BACKGROUND_KEY).setOrigin(0);
+        this.cursorText = this.add.text(0, 0, '', {color: 'black', fontSize: '2rem'});
+        this.originalSize = {width: this.scale.width, height: this.scale.height};
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.scale.on('resize', (gameSize: Phaser.Structs.Size) => {
+            console.log(gameSize);
+            const width = gameSize.width;
+            const height = gameSize.height;
+            this.cameras.resize(width, height);
+            this.background.setScale(width / this.originalSize.width, height / this.originalSize.height);
+        })
+
+        const resize = (changeX: number, changeY: number, lastKey: string) => {
+            this.game.scale.resize(this.game.canvas.width + changeX, this.game.canvas.height + changeY);
+            this.cursorText.text = lastKey;
+        }
+
+        this.cursors.up.on('up', () => {
+            resize(0, -100, 'up');
+        })
+        this.cursors.down.on('up', () => {
+            resize(0, 100, 'down');
+        })
+        this.cursors.left.on('up', () => {
+            resize(-100, 0, 'left');
+        })
+        this.cursors.right.on('up', () => {
+            resize(100, 0, 'right');
+        })
+        this.cursors.space.on('up', () => {
+            this.game.scale.resize(this.originalSize.width, this.originalSize.height);
+            this.background.setScale(1, 1);
+            this.cursorText.text = '';
+        })
     }
 }
 
